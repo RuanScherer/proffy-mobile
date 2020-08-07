9c1a9eb1671ffea7d5ed9c0c5b216577a106e1b6
Ruan Scherer <ruan.vscherer@gmail.com>
2020-08-07 09:18:35 -0300

    :sparkles: Get total connections from API

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Image, Text } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { RectButton } from 'react-native-gesture-handler'
-import styles from './styles'
 import landingImage from '../../assets/images/landing.png'
 import studyIcon from '../../assets/images/icons/study.png'
 import giveClassesIcon from '../../assets/images/icons/give-classes.png'
 import heartIcon from '../../assets/images/icons/heart.png'
+import api from '../../services/api'
+import styles from './styles'
 
 function Landing() {
+    const [totalConnections, setTotalConnections] = useState(0)
     const { navigate } = useNavigation()
+
+    useEffect(() => {
+        api.get('/connections')
+            .then(response => setTotalConnections(response.data.total))
+            .catch(err => console.log(err))
+    })
 
     function handleNavigateToGiveClassesPage() {
         navigate('GiveClasses')
+    }
+
+    function handleNavigateToStudyPage() {
+        navigate('Study')
     }
 
     return (
@@ -24,18 +36,22 @@ function Landing() {
             </Text>
 
             <View style={styles.buttonsContainer}>
-                <RectButton style={[styles.button, styles.buttonPrimary]}>
+                <RectButton 
+                    style={[styles.button, styles.buttonPrimary]}
+                    onPress={handleNavigateToStudyPage}>
                     <Image source={studyIcon}/>
                     <Text style={styles.buttonText}>Estudar</Text>
                 </RectButton>
-                <RectButton style={[styles.button, styles.buttonSecondary]} onPress={handleNavigateToGiveClassesPage}>
+                <RectButton 
+                    style={[styles.button, styles.buttonSecondary]}
+                    onPress={handleNavigateToGiveClassesPage}>
                     <Image source={giveClassesIcon}/>
                     <Text style={styles.buttonText}>Dar aulas</Text>
                 </RectButton>
             </View>
 
             <Text style={styles.totalConnections}>
-                Total de 200 conexões já realizadas {' '}
+                Total de {totalConnections} conexões já realizadas {' '}
                 <Image source={heartIcon} />
             </Text>
         </View>
