409c63fb9ae027f13c2609ddcaaf192ec48ac333
Ruan Scherer <ruan.vscherer@gmail.com>
2020-08-07 09:43:00 -0300

    :sparkles: Open whatsapp on press contact button

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { View, Image, Text } from 'react-native'
+import { View, Image, Text, Linking } from 'react-native'
 import { RectButton } from 'react-native-gesture-handler'
-import styles from './styles'
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png'
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png'
 import whatsappIcon from '../../assets/images/icons/whatsapp.png'
+import styles from './styles'
 
 export interface Teacher {
     name: string
@@ -22,6 +22,10 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function handleLinkToWhatsapp() {
+        Linking.openURL(`whatsapp://send?text=Hello World!&phone=${teacher.whatsapp}`)
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.profile}>
@@ -47,7 +51,7 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
                     <RectButton style={[styles.favoriteButton, styles.favorited]}>
                         <Image source={unfavoriteIcon} />
                     </RectButton>
-                    <RectButton style={styles.contactButton}>
+                    <RectButton style={styles.contactButton} onPress={handleLinkToWhatsapp}>
                         <Image source={whatsappIcon} />
                         <Text style={styles.contactButtonText}>
                             Entrar em contato
